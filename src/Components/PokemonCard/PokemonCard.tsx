7c8cebc013bc7a5655e686cb0aeeaa276a33f728
AsafG <asaf@interai.co>
2023-02-17 11:33:24 +0200

    Show Stats

--- src/Components/PokemonCard/PokemonCard.tsx
+++ src/Components/PokemonCard/PokemonCard.tsx
@@ -1,16 +1,44 @@
 import React from "react";
-import {Button, Card, CardActions, CardContent, Typography} from '@mui/material';
+import {Card, CardActions, CardContent, Collapse, IconButton, IconButtonProps, Typography} from '@mui/material';
 import classes from "./PokemonCard.module.scss";
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import {Pokemon} from "../../Models/Pokemon";
+import {styled} from '@mui/material/styles';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
+
 
 interface PokemonProps {
     pokemon: Pokemon
 }
 
+
+interface ExpandMoreProps extends IconButtonProps {
+    expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+    const {expand, ...other} = props;
+    return <IconButton {...other} />;
+})(({theme, expand}) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}));
+
+
 export const PokemonCard: React.FC<PokemonProps> = (
     {
         pokemon,
     }) => {
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
 
     const getTypes = (pokemon: Pokemon): string => {
         return pokemon.type_two ? `${pokemon.type_one} | ${pokemon.type_two}` : pokemon.type_one;
@@ -44,14 +72,32 @@ export const PokemonCard: React.FC<PokemonProps> = (
                         {getTypes(pokemon)}
                     </Typography>
                     <Typography variant="body2">
-                        well meaning and kindly.
-                        <br/>
-                        {'"a benevolent smile"'}
+                        Generation {pokemon.generation}
                     </Typography>
                 </CardContent>
                 <CardActions>
-                    <Button size="small">Learn More</Button>
+                    <IconButton aria-label="add to favorites">
+                        <FavoriteIcon/>
+                    </IconButton>
+                    <ExpandMore
+                        expand={expanded}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                    >
+                        <ExpandMoreIcon/>
+                    </ExpandMore>
                 </CardActions>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        <Typography className={classes.stats}>
+                            HP: {pokemon.hit_points} Attack: {pokemon.attack} Defense: {pokemon.defense} Sp. Attack: {pokemon.special_attack} Sp. Defense: {pokemon.special_defense} Speed: {pokemon.speed} Total: {pokemon.total}
+                        </Typography>
+                        <Typography className={classes.stats}>
+                            Legendary: {pokemon.legendary ? "Yes": "No"}
+                        </Typography>
+                    </CardContent>
+                </Collapse>
             </Card>
         </div>
     )
