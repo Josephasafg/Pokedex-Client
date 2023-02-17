5d845eb921cace2553bd8fa8bc7d7ea05d933a2c
AsafG <asaf@interai.co>
2023-02-17 09:38:49 +0200

    Added basic card

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.module.scss';
+import {PokemonCard} from "./Components/PokemonCard/PokemonCard";
+import classes from "./App.module.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className={classes.cardsContainer}>
+                <PokemonCard/>
+                <PokemonCard/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
