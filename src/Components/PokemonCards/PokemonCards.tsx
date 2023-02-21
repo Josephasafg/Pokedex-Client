e10bd705aae77f890a89d7c3073fb972764891b4
AsafG <asaf@interai.co>
2023-02-21 21:37:09 +0200

    Removed more css

--- src/Components/PokemonCards/PokemonCards.tsx
+++ src/Components/PokemonCards/PokemonCards.tsx
@@ -1,11 +1,17 @@
 import React from "react";
-import classes from "./PokemonCards.module.scss";
 import {PokemonCard} from "../PokemonCard/PokemonCard";
 import {Pokemon} from "../../Models/Pokemon";
+import styled from "styled-components";
 
 interface PokemonCardsProp {
     pokemons: Pokemon[]
 }
+
+const CardsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
 
 export const PokemonCards: React.FC<PokemonCardsProp> = (
     {
@@ -14,11 +20,11 @@ export const PokemonCards: React.FC<PokemonCardsProp> = (
 
 
     return (
-        <div className={classes.cardsContainer}>
+        <CardsWrapper>
             {pokemons.map((pokemon: Pokemon) => {
                 return <PokemonCard key={pokemon.pokemon_id} pokemon={pokemon}/>
             })}
 
-        </div>
+        </CardsWrapper>
     )
 }
