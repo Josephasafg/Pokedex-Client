5d3f05c59795c074273597c0e826eee369686e4b
AsafG <asaf@interai.co>
2023-02-21 12:13:04 +0200

    Removed title css

--- src/Components/StatsList/StatsList.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {List, Typography} from '@mui/material';
-import classes from "../PokemonCard/PokemonCard.module.scss";
-
-export const StatsList: React.FC = () => {
-    return (
-       <div></div>
-    )
-}
--- src/Components/Title/Title.tsx
+++ src/Components/Title/Title.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import classes from "./Title.module.scss";
+import styled from "styled-components";
 
+const TitleWrapper = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: baseline;
+  padding: 1em;
+  margin-bottom: 2em;
+  background-color: #313131;
+  color: #fff;
+`;
 export const Title: React.FC = () => {
     return (
-        <div className={classes.title}>
+        <TitleWrapper>
             <h1>My Pokédex</h1>
-        </div>
+        </TitleWrapper>
     )
 }
